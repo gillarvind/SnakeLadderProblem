fd2279049c8e7d7495decc7119349f8b2a0cad8e
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-24 13:48:32 +0530

    [arvind] Add: branch1 created

---
+++ SnakeLadderProb/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProb
+{
+    internal class Game
+    {
+        int PlayerPosition = 0;
+    }
+}
--- SnakeLadderProb/Program.cs
+++ SnakeLadderProb/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Snake and Ladder Problem Statement ");
         }
     }
 }
