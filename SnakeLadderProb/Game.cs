644c61d96f982c994527f0a4a900e24a7f20528d
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-24 14:43:33 +0530

    [arvind] Add: branch3 created

--- SnakeLadderProb/Game.cs
+++ SnakeLadderProb/Game.cs
@@ -9,11 +9,29 @@ namespace SnakeLadderProb
     internal class Game
     {
         int PlayerPosition = 0;
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        Random random = new Random();
         public int  DieRoll()
         {
-            Random random = new Random();
             int dieCount = random.Next(1, 7);
             return dieCount;
+        }
+        public void Play()
+        {
+            int Option = random.Next(0, 3);
+            switch (Option)
+            {
+                case NO_PLAY:
+                    PlayerPosition += 0;
+                    break;
+                case LADDER:
+                    PlayerPosition +=DieRoll();
+                    break;
+                case SNAKE:
+                    PlayerPosition -=DieRoll();
+                    break;
+            }
+            Console.WriteLine(PlayerPosition);
         }
     }    
 }
